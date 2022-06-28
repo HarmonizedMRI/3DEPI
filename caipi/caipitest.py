c391695c7368607f8e82dd11971fd2d6bcd5d97e
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-06-28 11:04:43 -0400

    Initial commit On branch caipi 	new file:   README.md 	renamed:    ../src/sampling/skippedcaipi_sampling.py -> caipi/skippedcaipi_sampling.py 	renamed:    ../src/sampling/test.py -> caipi/test.py 	new file:   caipitest.py 	new file:   skippedcaipi.py 	new file:   skippedcaipi_sampling.py 	deleted:    ../src/sampling/README.md 	deleted:    caipi/skippedcaipi_sampling.py 	deleted:    caipi/test.py

---
+++ caipi/caipitest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
+#from skippedcaipi import elementary_sampling, get_trajectory_indices, plot_parabola_connection, get_zblips, get_zblipcycle
+
+import skippedcaipi_sampling
+
+Ry=1 # Undersampling factor along y (primary phase encode direction, w.l.o.g.)
+Rz=6 # Undersampling factor along z (slice direction, w.l.o.g.)
+Dz=2 # CAIPI shift along z
+S=1  # Segmentation factor (typically 1, for fMRI)
+
+matrix_size_y = 64
+matrix_size_z = 30
+
+# Create the blipped-CAIPI instance linked to the CAIPI pattern (Segmentation Factor = 1)
+blippedcaipi = skippedcaipi_sampling.skippedcaipi_sampling(matrix_size_y, matrix_size_z, Ry, Rz, Dz, SegmentationFactor=S)
+
+plt.figure(figsize=(5,10))
+blippedcaipi.plot()
+plt.title('The blipped-CAIPI instance')
+plt.show()
